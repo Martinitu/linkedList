93cea073d8cfdfdf25868f649aa58564a2ff75dc
Martinitu <martinitu@gmail.com>
2023-03-13 17:20:19 -0500

    node and list classes added

---
+++ linkedList.js
@@ -0,0 +1,17 @@
+
+class node {
+    constructor(value = null, next = null){
+       node.data = data;
+       ListNode.next = next;
+    }
+}
+
+class linkedList {
+    constructor(head = null){
+        this.head = head
+    }
+}
+
+const list = new linkedList();
+
+console.log(list)
